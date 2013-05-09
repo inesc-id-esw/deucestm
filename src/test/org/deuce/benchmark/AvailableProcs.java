1c4f9126e3c72bc1bf7ffb636e37b17ab2b672c0
Fernando Miguel Carvalho <mcarvalho@cc.isel.ipl.pt>
2013-05-09 12:34:52 +0100

    New build.benchmarks.xml to execute Deuce benchmarks
    * build.benchmarks.xml => includes the main configurations for the execution
    of all benchmarks included in the Deuce distribution. For each benchmark
    provides both configurations with, and without, the runtime capture analysis
    activated.
    
    * AvailableMem and AvailableProcs => utility classes used by the
    build.benchmarks.xml.

---
+++ src/test/org/deuce/benchmark/AvailableProcs.java
@@ -0,0 +1,7 @@
+package org.deuce.benchmark;
+
+public class AvailableProcs{
+    public static void main(String[] args){
+            System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
